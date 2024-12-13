b423a8f17146ca23137b2763216d6a49b7e41443
karljayb <karl.bennett@gmail.com>
2024-12-13 07:40:45 +0000

    removed ts next.config file and replaces with mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const nextConfig: NextConfig = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
   experimental: {
     serverActions: {
