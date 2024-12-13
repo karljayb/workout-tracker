fd232729493b79e59e417d375df943002a0886dd
karljayb <karl.bennett@gmail.com>
2024-12-13 12:47:33 +0000

    updates to fix styling - test

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-red-500`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
